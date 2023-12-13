d3df97a39d8f7a58edff2800a653dbb572ac58d7
artem elfimov <artyomelfimov@mail.ru>
2023-12-13 17:30:04 +0300

    создание datagrid с фильтрами

---
+++ Components/FilteredDataGrid.cs
@@ -0,0 +1,65 @@
+﻿using DeviationModule.Commands;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DeviationModule.Components
+{
+    internal class FilteredDataGrid : DataGrid, INotifyPropertyChanged
+    {
+        public static ICommand? OpenFilterCommand { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public FilteredDataGrid()
+        {
+            OpenFilterCommand = new RelayCommand(IsOpenFilterExecuted);
+            Resources = new FilteredDataGridDictionary();
+
+        }
+        protected override void OnItemsSourceChanged(IEnumerable oldValue, IEnumerable newValue)
+        {
+            base.OnItemsSourceChanged(oldValue, newValue);
+            if (AutoGenerateColumns) {
+                OnCustomColumnCreate();
+            }
+        }
+        protected void OnCustomColumnCreate()
+        {
+            foreach (var item in Columns)
+            {
+                if (item != null && item.GetType() == typeof(DataGridTextColumn)) {
+                    ((DataGridTextColumn)item).HeaderTemplate = (DataTemplate)FindResource("ColTemp");
+
+                }
+
+            }
+        }
+        public void IsOpenFilterExecuted(object p)
+        {
+        }
+    }
+    /// <summary>
+    ///     ResourceDictionary
+    /// </summary>
+    public partial class FilteredDataGridDictionary
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// FilterDataGrid Dictionary
+        /// </summary>
+        public FilteredDataGridDictionary()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Public Constructors
+    }
+}
--- ViewModels/PositionViewModel.cs
+++ ViewModels/PositionViewModel.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using DeviationModule.Commands;
 using DeviationModule.Models;
 
 namespace DeviationModule.ViewModel
 {
     internal class PositionViewModel : ViewModelBase
     {
+
         public ObservableCollection<Deviation> Deviations  { get; set; }
         public PositionViewModel(ApplicationViewModel model)
         {
             Deviations = new ObservableCollection<Deviation>();
             Deviations = model.SelectedItem?.Deviations ?? new ObservableCollection<Deviation>();
-           
         }
+
     }
 }
